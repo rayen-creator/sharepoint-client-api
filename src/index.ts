fd17361a6b4a097a16e2120584aa9c4a33c618a8
rayen oueslati <rayenoueslati60@gmail.com>
2025-11-01 15:20:44 +0100

    fix : fix exported modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export * from "./client";
 export * from "./connect";
-export * from "./interfaces/initial-stage";
-export * from "./interfaces/configured-stage";
-export * from "./enums/http-method";
